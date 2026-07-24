c293ae24a4ad4c09ae5cf54b986417d81ebed876
Tim De Mey <tim.demey@dekimo.com>
2026-07-24 16:50:21 +0200

    Fixed flag language selector

--- src/app/components/language-selector/language-selector.component.ts
+++ src/app/components/language-selector/language-selector.component.ts
@@ -3,16 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { L10nService, SupportedLanguage, LanguageOption, LANGUAGES } from '../../services/l10n.service';
 import { L10nPipe } from '../../pipes/l10n.pipe';
+import { ClickOutsideDirective } from '../../directives/click-outside.directive';
 
 @Component({
   selector: 'app-language-selector',
   standalone: true,
-  imports: [CommonModule, FormsModule, L10nPipe],
+  imports: [CommonModule, FormsModule, L10nPipe, ClickOutsideDirective],
   templateUrl: './language-selector.component.html',
   styleUrl: './language-selector.component.css'
 })
 export class LanguageSelectorComponent {
   languages: LanguageOption[] = LANGUAGES;
+  isOpen = false;
 
   constructor(public l10n: L10nService) {}
 
@@ -20,8 +22,20 @@ export class LanguageSelectorComponent {
     return this.l10n.currentLanguage;
   }
 
-  onLanguageChange(event: Event): void {
-    const select = event.target as HTMLSelectElement;
-    this.l10n.setLanguage(select.value as SupportedLanguage);
+  get currentLanguageOption(): LanguageOption | undefined {
+    return this.languages.find(l => l.code === this.currentLanguage);
+  }
+
+  toggleDropdown(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  closeDropdown(): void {
+    this.isOpen = false;
+  }
+
+  selectLanguage(lang: LanguageOption): void {
+    this.l10n.setLanguage(lang.code);
+    this.isOpen = false;
   }
 }
